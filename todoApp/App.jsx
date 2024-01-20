a3ace06ce47a529f44f92073df1bd003818ab0cb
Deepak <deepak2603dk@gmail.com>
2024-01-20 17:04:31 +0530

    react native hello world code

---
+++ todoApp/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize:30}}>First program</Text>
+      <Text style={{fontSize:20}}>Hello, World!</Text>
+    </View>
+  );
+};
+
+export default App;

b17229cb213d2385451b4b3cbf3d8d8b64e4355f
Deepak <deepak2603dk@gmail.com>
2024-01-26 17:35:16 +0530

    passing data from one page to another

--- todoApp/component/navigation/stack/Home.jsx
+++ todoApp/component/navigation/stack/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-export const Home = () => {
+export const Home = (props) => {
+    const { userData } = props.route.params;
+
   return (
     <View style={{flex:1,justifyContent:"center", alignItems:"center"}}>
         <Text>Home page</Text>
+        <Text>{userData.name}</Text>
+        <Text>{userData.email}</Text>
     </View>
   )
 }
--- todoApp/component/navigation/stack/Login.jsx
+++ todoApp/component/navigation/stack/Login.jsx
@@ -4,10 +4,15 @@ import { Button, Text, View } from 'react-native'
 export const Login = (props) => { 
     //navigator automaticaly give props about all screens
     // console.log('>>>>>>>>>>>', props)
+    const userData = {
+        name : "Deepak",
+        email : "deepak@gmail.com"
+    }
   return (
     <View style={{flex:1,justifyContent:"center", alignItems:"center"}}>
     <Text>Login page</Text>
-    <Button title='go to Home page' onPress={()=> props.navigation.navigate("Home")} />
+    <Button title='go to Home page' 
+    onPress={()=> props.navigation.navigate("Home", {userData})} />
 </View>
   )
 }

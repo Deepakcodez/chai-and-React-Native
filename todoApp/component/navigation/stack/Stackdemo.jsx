fbf31632df451c083b58e8271a7895c316e623eb
Deepak <deepak2603dk@gmail.com>
2024-01-26 16:57:25 +0530

    navigation styling

--- todoApp/component/navigation/stack/Stackdemo.jsx
+++ todoApp/component/navigation/stack/Stackdemo.jsx
@@ -8,13 +8,44 @@ import { Login } from './Login';
 
 
 export const Stackdemo = () => {
-  const Stack = createNativeStackNavigator();
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='Home' component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    const Stack = createNativeStackNavigator();
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+            // this apply to all screen  navbar 
+            screenOptions={
+                {
+                   
+                    headerStyle: {
+                        backgroundColor: "pink"
+                    },
+                    headerTintColor: "black",
+                    headerTitleStyle: {
+                        fontSize: 20,
+                    }
+
+                }}
+            >
+                <Stack.Screen name='Login'
+                    options={
+                        {
+                            title: "user Login",
+                            headerStyle: {
+                                backgroundColor: "pink"
+                            },
+                            headerTintColor: "black",
+                            headerTitleStyle: {
+                                fontSize: 30,
+                            }
+
+                        }}
+
+                    component={Login} />
+
+                <Stack.Screen name='Home' component={Home} />
+
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };

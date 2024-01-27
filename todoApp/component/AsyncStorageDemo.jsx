5462665f392c63480a113a2c0b6cb243909cb4ce
Deepak <deepak2603dk@gmail.com>
2024-01-27 18:05:04 +0530

    firebase setup in react native

--- todoApp/component/AsyncStorageDemo.jsx
+++ todoApp/component/AsyncStorageDemo.jsx
@@ -3,16 +3,16 @@ import { Button, Text, TextInput, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AsyncStorageDemo = () => {
-   const [data , setData] =useState("");
-   const [show,setShow] = useState(false)
+    const [data, setData] = useState("");
+    const [show, setShow] = useState(false)
 
 
-   
-   const setDataHandler=(text)=>{
-       setShow(false)
-       setData(text)
-       
-}
+
+    const setDataHandler = (text) => {
+        setShow(false)
+        setData(text)
+
+    }
 
     const setHandler = async () => {
         try {
@@ -22,9 +22,9 @@ export const AsyncStorageDemo = () => {
             console.error("Error setting data in AsyncStorage:", error);
         }
     };
-    
+
     const getHandler = async () => {
-        setShow(true)  
+        setShow(true)
         try {
             const name = await AsyncStorage.getItem("name");
             console.log("Retrieved data from AsyncStorage:", name);
@@ -41,9 +41,9 @@ export const AsyncStorageDemo = () => {
     return (
         <View>
             <Text style={{ fontSize: 30 }}>Async storage in React Native</Text>
-            <TextInput placeholder='enter something' 
-            onChangeText={(text)=>setDataHandler(text)}
-            style={{borderColor:"black", borderWidth:1, borderRadius:10, paddingLeft:10}}
+            <TextInput placeholder='enter something'
+                onChangeText={(text) => setDataHandler(text)}
+                style={{ borderColor: "black", borderWidth: 1, borderRadius: 10, paddingLeft: 10 }}
             />
             <View style={{ margin: 10 }}>
                 <Button title='Set Data' onPress={setHandler} />
@@ -51,10 +51,10 @@ export const AsyncStorageDemo = () => {
             <View style={{ margin: 10 }}>
                 <Button title='Get Data' onPress={getHandler} />
             </View>
-            <View style={{height:200, backgroundColor:"black",borderRadius:10}}>
+            <View style={{ height: 200, backgroundColor: "black",padding:20, borderRadius: 10 }}>
                 {
-                    show?
-                    <Text style={{color:"pink"}}>{data}</Text>:null
+                    show ?
+                        <Text style={{ color: "pink" }}>{data}</Text> : null
                 }
             </View>
 

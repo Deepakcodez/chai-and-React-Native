38346d08b3dea37ef532883c964e471404a8fdc3
Deepak <deepak2603dk@gmail.com>
2024-01-30 22:57:39 +0530

    doing some changes

--- reactNativeWithFirebase/screens/Auth/Createuser.jsx
+++ reactNativeWithFirebase/screens/Auth/Createuser.jsx
@@ -1,34 +1,40 @@
 import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import auth from '@react-native-firebase/auth';
+import { useNavigation } from '@react-navigation/native';
+import Login from './Login';
 
 
 
 const Createuser = () => {
   const [email, setEmail] = useState(null)
   const [pass, setPass] = useState(null)
-  const [msg,setMsg] = useState("")
+  const [msg, setMsg] = useState("")
+  const navigation = useNavigation()
 
-  
-  const handleCreate = async()=>{
+  const handleCreate = async () => {
     try {
       const isuserCreated = await auth().createUserWithEmailAndPassword(email, pass)
       setEmail("")
       setPass("")
 
-      
+
     } catch (error) {
       console.log(error);
       setMsg(error.message)
-      
+
     }
+  }
+
+  const loginPress = () => {
+navigation.navigate("login")
   }
 
 
 
   return (
     <View style={styles.cont}>
-      <Text style={{textAlign : "center" , fontSize : 30}}>Register</Text>
+      <Text style={{ textAlign: "center", fontSize: 30 }}>Register</Text>
       <TextInput
         style={styles.input}
         placeholder='enter email'
@@ -50,6 +56,10 @@ const Createuser = () => {
           </Text>
         </View>
       </TouchableOpacity>
+      <View>
+
+        <Text style={styles.logintext} >already have an account ? <Text onPress={loginPress} style={{ color: "blue" }}>Login</Text> </Text>
+      </View>
 
       <Text>{msg}</Text>
 
@@ -67,21 +77,27 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
 
   },
-  btn:{
-    backgroundColor:"green",
-    padding : 12,
-    borderRadius : 40,
-    margin : 20
+  btn: {
+    backgroundColor: "green",
+    padding: 12,
+    borderRadius: 40,
+    margin: 20
 
 
 
   },
-  btnText:{
+  btnText: {
     color: "white",
-    fontSize : 20,
-    textAlign : "center"
-,
+    fontSize: 20,
+    textAlign: "center"
+    ,
 
   },
+  cont: {
+
+  },
+  logintext: {
+    textAlign: "center"
+  }
 
 })

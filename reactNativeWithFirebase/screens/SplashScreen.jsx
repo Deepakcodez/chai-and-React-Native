e2a4313ebce2f190b07cea5d37d444d2430236b1
Deepak <deepak2603dk@gmail.com>
2024-02-04 23:34:05 +0530

    animated splash screen

--- reactNativeWithFirebase/screens/SplashScreen.jsx
+++ reactNativeWithFirebase/screens/SplashScreen.jsx
@@ -2,6 +2,8 @@ import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
 import Auth from '@react-native-firebase/auth';
 import { StackActions, useNavigation } from '@react-navigation/native';
+import * as Animatable from 'react-native-animatable';
+import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 
 const SplashScreen = () => {
     const navigation = useNavigation()
@@ -13,11 +15,45 @@ const SplashScreen = () => {
                 navigation.dispatch(StackActions.replace(gotoScreen))
                 
             })
-        }, 2000);
+        }, 3000);
     },[])
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={{flex:1,justifyContent:"center", alignItems:"center", backgroundColor:"white"}}>
+       <Animatable.Image
+       source={require('../Assets/logo.png')}
+       style={{height:responsiveHeight(15),
+      width:responsiveWidth(25)}}
+      animation={"zoomIn"}
+       />
+       <Animatable.View
+       style={{height:responsiveHeight(35),
+      width:responsiveHeight(35),
+      borderRadius:responsiveHeight(50),
+      backgroundColor:"#5687ff",
+      position:"absolute",
+      top:-54,
+      left:-100,
+
+    }}
+    animation={"zoomIn"}
+       >
+
+       </Animatable.View>
+       <Animatable.View
+       style={{height:responsiveHeight(35),
+      width:responsiveHeight(35),
+      borderRadius:responsiveHeight(50),
+      backgroundColor:"#5687ff",
+      opacity:.5,
+      position:"absolute",
+      bottom:-94,
+      right:-150,
+
+    }}
+    animation={"slideInUp"}
+       >
+
+       </Animatable.View>
     </View>
   )
 }
